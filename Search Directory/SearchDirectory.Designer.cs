0d58552fa016f32d93b4c4e911fd40a5c0c5ee83
Pavan18 <pavan18@gmail.com>
2016-01-31 13:06:23 -0800

    commit

---
+++ Search Directory/SearchDirectory.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Search_Directory
+{
+    partial class SearchDirectory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SearchDirectory));
+            this.lblExtension = new System.Windows.Forms.Label();
+            this.txtFoler = new System.Windows.Forms.TextBox();
+            this.lblDirectory = new System.Windows.Forms.Label();
+            this.cboDirectory = new System.Windows.Forms.ComboBox();
+            this.lstFoundFiles = new System.Windows.Forms.ListBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.chkLookinAll = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lblExtension
+            // 
+            this.lblExtension.AutoSize = true;
+            this.lblExtension.Location = new System.Drawing.Point(14, 18);
+            this.lblExtension.Name = "lblExtension";
+            this.lblExtension.Size = new System.Drawing.Size(91, 13);
+            this.lblExtension.TabIndex = 0;
+            this.lblExtension.Text = "Search for folder :";
+            // 
+            // txtFoler
+            // 
+            this.txtFoler.Location = new System.Drawing.Point(17, 43);
+            this.txtFoler.Name = "txtFoler";
+            this.txtFoler.Size = new System.Drawing.Size(166, 20);
+            this.txtFoler.TabIndex = 1;
+            this.txtFoler.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtExtension_KeyDown);
+            // 
+            // lblDirectory
+            // 
+            this.lblDirectory.AutoSize = true;
+            this.lblDirectory.Location = new System.Drawing.Point(14, 122);
+            this.lblDirectory.Name = "lblDirectory";
+            this.lblDirectory.Size = new System.Drawing.Size(113, 13);
+            this.lblDirectory.TabIndex = 2;
+            this.lblDirectory.Text = "Look in Logical Drive :";
+            // 
+            // cboDirectory
+            // 
+            this.cboDirectory.FormattingEnabled = true;
+            this.cboDirectory.Location = new System.Drawing.Point(13, 148);
+            this.cboDirectory.Name = "cboDirectory";
+            this.cboDirectory.Size = new System.Drawing.Size(170, 21);
+            this.cboDirectory.TabIndex = 3;
+            this.cboDirectory.KeyDown += new System.Windows.Forms.KeyEventHandler(this.cboDirectory_KeyDown);
+            // 
+            // lstFoundFiles
+            // 
+            this.lstFoundFiles.FormattingEnabled = true;
+            this.lstFoundFiles.Location = new System.Drawing.Point(256, 18);
+            this.lstFoundFiles.Name = "lstFoundFiles";
+            this.lstFoundFiles.Size = new System.Drawing.Size(546, 251);
+            this.lstFoundFiles.TabIndex = 5;
+            this.lstFoundFiles.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lstFoundFiles_KeyDoubleClick);
+            this.lstFoundFiles.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lstFoundFiles_MouseDoubleClick);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(727, 293);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // chkLookinAll
+            // 
+            this.chkLookinAll.AutoSize = true;
+            this.chkLookinAll.Location = new System.Drawing.Point(13, 175);
+            this.chkLookinAll.Name = "chkLookinAll";
+            this.chkLookinAll.Size = new System.Drawing.Size(160, 17);
+            this.chkLookinAll.TabIndex = 4;
+            this.chkLookinAll.Text = "Look in all Active Directories";
+            this.chkLookinAll.UseVisualStyleBackColor = true;
+            this.chkLookinAll.CheckedChanged += new System.EventHandler(this.chkLookinAll_CheckedChanged);
+            this.chkLookinAll.KeyDown += new System.Windows.Forms.KeyEventHandler(this.chkLookinAll_KeyDown);
+            // 
+            // SearchDirectory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 338);
+            this.Controls.Add(this.chkLookinAll);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lstFoundFiles);
+            this.Controls.Add(this.cboDirectory);
+            this.Controls.Add(this.lblDirectory);
+            this.Controls.Add(this.txtFoler);
+            this.Controls.Add(this.lblExtension);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SearchDirectory";
+            this.Text = "Search Directory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblExtension;
+        private System.Windows.Forms.TextBox txtFoler;
+        private System.Windows.Forms.Label lblDirectory;
+        private System.Windows.Forms.ComboBox cboDirectory;
+        private System.Windows.Forms.ListBox lstFoundFiles;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.CheckBox chkLookinAll;
+    }
+}
+
---
+++ Search Directory/SearchDirectory.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Search_Directory
+{
+    public partial class SearchDirectory : Form
+    {
+        private string formName = "Search Directory";
+        private string formNameLoading = "Search Directory.. (Please Wait !!!...)";
+
+        public string SearchDirectoryName { get; set; }
+ 
+        public SearchDirectory()
+        {
+            InitializeComponent();
+            PopulateActiveDirectories();
+        }
+
+        private void PopulateActiveDirectories()
+        {
+            cboDirectory.Items.Clear();
+            foreach(string directory in Directory.GetLogicalDrives())
+            {
+                cboDirectory.Items.Add(directory);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (!ValidateForm())
+                return;
+
+            SearchDirectoryName = cboDirectory.Text;
+            lstFoundFiles.Items.Clear();
+            txtFoler.Enabled = false;
+            cboDirectory.Enabled = false;
+            chkLookinAll.Enabled = false;
+            this.Cursor = Cursors.WaitCursor;
+            this.Text = formNameLoading;
+            Application.DoEvents();
+
+            if (chkLookinAll.Checked)
+                SearchInAllDirectiries();
+            else
+                SearchInDirectory(SearchDirectoryName);
+
+            lstFoundFiles.Focus();
+            this.Cursor = Cursors.Default;
+            this.Text = formName;
+            txtFoler.Enabled = true;
+            cboDirectory.Enabled = true;
+            chkLookinAll.Enabled = true;
+        }
+
+        private bool ValidateForm()
+        {
+            if(String.IsNullOrEmpty(txtFoler.Text))
+            {
+                MessageBox.Show("Enter Directory name to search", "Search Directory", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtFoler.Focus();
+                return false;
+            }
+
+            if(String.IsNullOrEmpty(cboDirectory.Text) && !chkLookinAll.Checked)
+            {
+                MessageBox.Show("Select a Logical Drive", "Invalid Drive", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                cboDirectory.Focus();
+                return false;
+            }
+
+            if (!Directory.Exists(cboDirectory.Text) && !chkLookinAll.Checked)
+            {
+                MessageBox.Show("Logical Drive does not Exist.", "Invalid Drive", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                cboDirectory.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void SearchInAllDirectiries()
+        {
+            DialogResult dialogResult = MessageBox.Show("Searching in all Drives will take several minutes. Do you want to continue?", "Search Directory", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (dialogResult == System.Windows.Forms.DialogResult.No)
+            {
+                chkLookinAll.Focus();
+                return;
+            }
+
+            foreach(string logicalDrive in Directory.GetLogicalDrives())
+            {
+                SearchInDirectory(logicalDrive);
+            }
+        }
+
+        private void SearchInDirectory(string SearchDirectoryName)
+        {
+            
+                if(!Directory.Exists(SearchDirectoryName))
+                    return;
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(SearchDirectoryName);
+                List<DirectoryInfo> directoryNames = directoryInfo.EnumerateDirectories().ToList();
+                foreach (DirectoryInfo directory in directoryNames)
+                {
+                    try
+                    {
+                        SearchInDirectory(directory.FullName);
+                        if (directory.Name.IndexOf(txtFoler.Text, 0, StringComparison.CurrentCultureIgnoreCase) != -1)
+                            lstFoundFiles.Items.Add(directory.FullName);
+                    }
+                    catch(UnauthorizedAccessException)
+                    {
+                        // Do Nothing.
+                        continue;
+                    }
+                }
+        }
+
+        private void lstFoundFiles_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo()
+            {
+                FileName = lstFoundFiles.Text,
+                UseShellExecute = true,
+                Verb = "open"
+            });
+        }
+
+        private void chkLookinAll_CheckedChanged(object sender, EventArgs e)
+        {
+            if(!chkLookinAll.Checked)
+            {
+                cboDirectory.Enabled = true;
+                return;
+            }
+            
+            cboDirectory.Enabled = false;            
+        }
+
+        private void txtExtension_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                btnSearch_Click(sender, e);
+            }
+        }
+
+        private void chkLookinAll_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnSearch_Click(sender, e);
+            }
+        }
+
+        private void cboDirectory_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnSearch_Click(sender, e);
+            }
+        }
+
+        private void lstFoundFiles_KeyDoubleClick(object sender, KeyEventArgs e)
+        {
+            if (string.IsNullOrEmpty(lstFoundFiles.Text) || e.KeyCode != Keys.Enter)
+                return;
+
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo()
+            {
+                FileName = lstFoundFiles.Text,
+                UseShellExecute = true,
+                Verb = "open"
+            });
+        }
+        
+    }
+}
